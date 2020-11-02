d5890b28eed4a4fdab58091128e2639a83d2ce25
Alex <cigan@ua.fm>
2020-11-02 12:48:32 +0200

    add the structure of the project

---
+++ my-first-app/src/components/app-header/app-header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AppHeader = () => {
+    return (
+        <div className="app-header d-flex">
+            <h1>Olexander Tsyhan</h1>
+            <h2>5 записей, из низ понравилось 0</h2>
+        </div>
+    )
+}
+
+export default AppHeader;
---
+++ my-first-app/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/app/';   // если не указать какой именно файл, по умолчанию будет искать index.js
+
+ReactDOM.render(
+  <App />, document.getElementById('root')
+);
