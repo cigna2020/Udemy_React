11c6f218782b6fdbce92823d83a09ba31a47897a
Alex <cigan@ua.fm>
2020-11-03 20:21:27 +0200

    add a method to add on the page new post with the world 'Hello'

--- my-first-app/src/components/app/app.js
+++ my-first-app/src/components/app/app.js
@@ -35,6 +35,9 @@ export default class App extends Component {
             ]
         };
         this.deleteItem = this.deleteItem.bind(this);
+        this.addItem = this.addItem.bind(this);
+
+        this.maxId = 4;
     }
 
     deleteItem(id) {
@@ -52,6 +55,20 @@ export default class App extends Component {
         });
     }
 
+    addItem(body) {
+        const newItem = {
+            label: body,
+            important: false,
+            id: this.maxId++
+        }
+        this.setState(({ data }) => {
+            const newArr = [...data, newItem];
+            return {
+                data: newArr
+            }
+        })
+    }
+
     render() {
         return (
             <AppBlock>
@@ -63,7 +80,8 @@ export default class App extends Component {
                 <PostList
                     posts={this.state.data}
                     onDelete={this.deleteItem} />
-                <PostAddForm />
+                <PostAddForm
+                    onAdd={this.addItem} />
             </AppBlock >
         )
 
